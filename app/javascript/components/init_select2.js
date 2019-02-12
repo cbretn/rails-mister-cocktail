be709386a0a8c740004fcca545ba850587236723
Charles Breton <charles.bretn@gmail.com>
2019-02-12 17:09:05 +0800

    fiexd type.js & select2 issues

--- app/javascript/components/init_select2.js
+++ app/javascript/components/init_select2.js
@@ -1,15 +1,14 @@
 import $ from 'jquery';
 import 'select2';
 
-// const initSelect2 = () => {
-//   $('.select2').select2();
-//   console.log('hello from select2');
-// };
+const initSelect2 = () => {
+  $(document).ready(function() {
+    $('.select2').select2();
+    console.log('hello from select2');
+  })
+};
 
-$(document).ready(function() {
-  $('.select2').select2();
-  console.log('hello from select2');
-})
+
 
 export default initSelect2;
 
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,8 +15,6 @@ import 'select2/dist/css/select2.css';
 
 import initSelect2 from '../components/init_select2';
 
-initSelect2();
-
 import { typed } from '../components/init_typed';
 
 typed();
